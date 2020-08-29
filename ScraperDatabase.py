f7cac51893399feabcdcabb1cf6aa0fab251d353
Malte Niedereichholz <malte@Maltes-Air.fritz.box>
2020-08-29 08:16:02 +0200

    prepare for mysql setup

--- ScraperDatabase.py
+++ ScraperDatabase.py
@@ -1,22 +1,10 @@
-import sqlite3
+import mysql.connector as mysql
 
-connection = sqlite3.connect('scraper.db')
-cursor = connection.cursor()
-
-create_table_products = 'CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY, title TEXT)'
-
-
-def create_table(query: str):
-    cursor.execute(query)
-    connection.commit()
+db = mysql.connect(
+    host='localhost',
+    user='root',
+    password='root'
+)
 
 
-create_table(create_table_products)
-
-
-def insert_product(title: str):
-    cursor.execute('INSERT INTO products(title) VALUES (?);', title)
-    connection.commit()
-
-
-insert_product('Testprodukt')
+print(db)
